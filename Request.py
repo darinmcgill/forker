4092ce17f196e8710313b01753a1fc20b4573276
Darin McGill <darin.mcgill@gmail.com>
2016-11-20 02:14:58 -0500

    various refactorings

---
+++ Request.py
@@ -0,0 +1,277 @@
+from __future__ import print_function
+import re
+import sys
+import datetime
+import os
+import socket
+import base64
+import glob
+import stat
+import unittest
+
+
+class Request(object):
+    __slots__ = ("request_id", "remote_ip", "protocol", "method",
+                 "requested_path", "headers", "cookies", "body", "query_string", )
+
+    OK = b"HTTP/1.0 200 OK\r\n"
+
+    def __init__(self, **kwargs):
+        """
+            Request(sock, remote_ip=None, request_id=None)
+             or
+            Request(<contents>)
+        """
+        if "sock" not in kwargs:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        else:
+            sock = kwargs["sock"]
+            self.remote_ip = kwargs.get("remote_ip")
+            self.request_id = kwargs.get("request_id")
+            buff = b""
+            match = None
+            while not match:
+                buff += sock.recv(4096)
+                match = re.match(b"(.*?)\\r?\\n\\r?\\n(.*)", buff, re.S)
+            header_block = match.group(1)
+            if not isinstance(header_block, str):
+                header_block = header_block.decode()
+            self.body = match.group(2)
+            lines = re.split("\\r?\\n", header_block)
+            first = lines.pop(0)
+            self.method, request_string, self.protocol = first.split()
+            self.requested_path = request_string.split("?")[0]
+            self.query_string = request_string.split("?", 1)[1] if "?" in request_string else ""
+            self.headers = dict()
+            for line in lines:
+                a, b = line.split(":", 1)
+                self.headers[a.strip().lower()] = b.strip()
+            self.cookies = dict()
+            if "cookie" in self.headers:
+                for cookie in re.split(";\s*", self.headers["cookie"]):
+                    k, v = cookie.strip().split("=")
+                    self.cookies[k] = v
+            if "content-length" in self.headers:
+                while len(self.body) < int(self.headers["content-length"]):
+                    self.body += sock.recv(4096)
+
+    def render(self, start, sep, end):
+        out = start
+        for key in self.__slots__:
+            out += "%s=%r%s" % (key, getattr(self, key), sep)
+        out += end
+        return out
+
+    def __repr__(self):
+        return self.render(sep=",", start="Request(", end=")")
+
+    def __str__(self):
+        bar = ("-" * 40) + "\n"
+        return self.render(sep="\n\n", start=bar, end=bar)
+
+    def __bytes__(self):
+        out = str(self)
+        if not isinstance(out, bytes):
+            out = out.encode()
+        return out
+
+    def log(self, file=sys.stdout):
+        ts = str(datetime.datetime.now())
+        print(repr([ts, self.requested_path, self.method, self.remote_ip]), file=file)
+
+    def cgi(self, resolved):
+        for k in os.environ.keys():
+            if k in ["PATH", "PYTHONPATH"]:
+                continue
+            del os.environ[k]
+        if self.query_string:
+            os.environ["QUERY_STRING"] = self.query_string
+        os.environ["SERVER_SOFTWARE"] = "forker.py/20150821"
+        os.environ["SERVER_NAME"] = socket.gethostname()
+        # os.environ["SERVER_PORT"] = str(self.port)
+        os.environ["SERVER_PROTOCOL"] = "HTTP/1.0"
+        os.environ["CONTENT_LENGTH"] = str(len(self.body)) if self.body else ""
+        os.environ["GATEWAY_INTERFACE"] = "CGI/1.1"
+        os.environ["REQUEST_METHOD"] = self.method
+        os.environ["SCRIPT_NAME"] = self.requested_path
+        os.environ["HTTP_USER_AGENT"] = self.headers.get("user-agent", "")
+        os.environ["HTTP_ACCEPT_LANGUAGE"] = self.headers.get("accept-language", "")
+        os.environ["HTTP_COOKIE"] = self.headers.get("cookie", "")
+        os.environ["HTTP_REFERER"] = self.headers.get("referer", "")
+        os.environ["CONTENT_TYPE"] = self.headers.get("content-type", "")
+        os.environ["HTTP_HOST"] = self.headers.get("host", "")
+        os.environ["REMOTE_ADDR"] = self.remote_ip
+        os.environ["HTTP_X_REAL_IP"] = self.headers.get("x-real-ip", "")
+        os.environ["HTTP_X_FORWARDED_FOR"] = self.headers.get("x-forwarded-for", "")
+        os.environ["HTTP_X_FORWARDED_HOST"] = self.headers.get("x-forwarded-host", "")
+        os.environ["HTTP_X_FORWARDED_URI"] = self.headers.get("x-forwarded-uri", "")
+        os.environ["HTTP_X_FORWARDED_REQUEST_URI"] = self.headers.get(
+            "x-forwarded-request-uri", "")
+        if "Basic" in self.headers.get("authorization", ""):
+            second = self.headers["authorization"].split()[1]
+            user = base64.decodebytes(second).split(":")[0]
+            os.environ["HTTP_X_AUTH_USER"] = user
+        from subprocess import Popen, PIPE
+        child = Popen(resolved, stdin=PIPE, stdout=PIPE)
+        out, err = child.communicate(self.body or b"")
+        if child.returncode != 0:
+            out = b"HTTP/1.0 500 Error\r\n"
+            out += b"Content-type: text/plain\r\n\r\n"
+            out += b"non-zero return code\n\n"
+            return out
+        m = re.match(b"^(.*?)\\n\\r?\\n(.*)$", out, re.S)
+        if not m:
+            return b"HTTP/1.0 500 Bad Header\r\n\r\n500 Bad Header"
+        header = m.group(1)
+        body = m.group(2)
+        lines = re.split(b"\\r?\\n", header)
+        status = Request.OK
+        header = ""
+        for line in lines:
+            if re.match(b"^status:$", line, re.I):
+                status = b"HTTP/1.0 " + line.split(b":")[1] + b"\r\n"
+            else:
+                header += line + b"\r\n"
+        return status + header + b"\r\n" + body
+
+    def serve(self):
+        try:
+            resolved = Request.resolve(self.requested_path, os.getcwd())
+        except KeyError:
+            return b"HTTP/1.0 404 Not Found\r\n\r\n404 Not Found"
+        if os.path.isdir(resolved):
+            if "x-forwarded-uri" in self.headers:
+                self.requested_path = self.headers["x-forwarded-uri"]
+            return Request.get_listing(resolved, self.requested_path)
+        if Request.executable(resolved):
+            return self.cgi(resolved)
+        if self.method == "GET":
+            return Request.OK + Request.type_line(resolved) + b"\r\n" + open(resolved, "rb").read()
+        return b"HTTP/1.0 500 Unexpected\r\n\r\n500 Unexpected\n" + bytes(self)
+
+    @staticmethod
+    def get_listing(resolved, raw):
+        print("getListing(%r,%r)" % (resolved, raw))
+        assert os.path.isdir(resolved)
+        out = b"HTTP/1.0 200 OK\r\n"
+        out += b"Content-type: text/html\r\n\r\n"
+        out += b"""
+        <html><head><title>directory listing</title>
+        <style>
+        pre {font-size: large;}
+        </style>
+        </head><body>
+        <pre>Contents:"""
+        if not raw.endswith("/"):
+            raw += "/"
+        for thing in glob.glob(resolved + "/*"):
+            if os.path.isdir(thing):
+                d = "/"
+            elif os.path.islink(thing):
+                d = "@"
+            elif Request.executable(thing):
+                d = "*"
+            else:
+                d = ""
+            last = os.path.basename(thing)
+            out += ("\n\t<a href='%s%s'>%s</a>%s" % (raw, last, last, d)).encode()
+        out += b"</pre></font></body></html>"
+        return out
+
+    @staticmethod
+    def resolve(path, relative):
+        print("resolve(%r,%r)" % (path, relative))
+        if not isinstance(path, list):
+            path = [p for p in path.split("/") if p]
+        assert isinstance(path, list)
+        assert os.path.exists(relative), relative
+        if os.path.islink(relative):
+            return Request.resolve(path, os.path.realpath(relative))
+        if os.path.isfile(relative):
+            return relative
+        assert os.path.isdir(relative), relative
+        if len(path) == 0:
+            try:
+                return Request.resolve(["index"], relative)
+            except:
+                return relative
+        name = path.pop(0)
+        contents = os.listdir(relative)  # does not include ".." and "."
+        if name in contents:
+            return Request.resolve(path, os.path.join(relative, name))
+        matches = [x for x in contents if x.startswith(name + ".")]
+        if len(matches) == 1:
+            return Request.resolve([], os.path.join(relative, matches[0]))
+        if len(matches) > 1:
+            raise KeyError("ambiguous:" + os.path.join(relative, name))
+        raise KeyError(os.path.join(relative, name))
+
+    @staticmethod
+    def executable(path):
+        mode = os.stat(path).st_mode
+        return bool(stat.S_IXOTH & mode)
+
+    @staticmethod
+    def readable(path):
+        mode = os.stat(path).st_mode
+        return bool(stat.S_IROTH & mode)
+
+    type_map = dict(
+        html=b"text/html",
+        htm=b"text/html",
+        css=b"text/css",
+        txt=b"text/plain",
+        xml=b"text/xml",
+        manifest=b"text/cache-manifest",
+        appcache=b"text/cache-manifest",
+        pdf=b"application/pdf",
+    )
+
+    @staticmethod
+    def type_line(fn):
+        for k, v in Request.type_map.items():
+            if fn.endswith("." + k):
+                return b"Content-type: " + v + b"\r\n"
+        return b""  # let the browser guess
+
+
+_example_request = b"""GET /abc?xyz HTTP/1.1
+Host: localhost:8080
+Upgrade-Insecure-Requests: 1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Encoding: gzip, deflate, sdch, br
+Accept-Language: en-US,en;q=0.8
+Cookie:trail=6231214290744395; scent=6457421329820405
+
+"""
+
+
+class TestRequest(unittest.TestCase):
+
+    def test_socket(self):
+        test_data = b"hello\nworld!"
+        client, server = socket.socketpair()
+        client.send(test_data)
+        buff = server.recv(4096)
+        self.assertEqual(buff, test_data)
+        client.close()
+        server.close()
+
+    def test_request(self):
+        client, server = socket.socketpair()
+        client.send(_example_request)
+        request = Request(sock=server)
+        client.close()
+        server.close()
+        self.assertEqual(request.method, "GET")
+        self.assertEqual(request.requested_path, "/abc")
+        self.assertEqual(request.query_string, "xyz")
+        self.assertEqual(request.headers["host"], "localhost:8080")
+        self.assertFalse(request.body)
+        self.assertEqual(request.cookies.get("scent"), "6457421329820405")
+        self.assertEqual(request.cookies.get("trail"), "6231214290744395")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ WebSocketServer.py
@@ -0,0 +1,199 @@
+from __future__ import print_function
+import hashlib
+import base64
+
+_magic = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+_sha1 = lambda x: hashlib.sha1(x).digest()
+_sign = lambda x: base64.encodebytes(_sha1(x + _magic)).strip()
+
+CONT  = 0
+TEXT  = 1
+BIN   = 2
+CLOSE = 8
+PING  = 9
+PONG  = 10
+
+
+def _unmask(payload,mask):
+    indv = list()
+    abcd = map(ord,mask)
+    for i in range(len(payload)):
+        j = i % 4
+        indv.append(chr(abcd[j] ^ ord(payload[i])))
+    return "".join(indv)
+
+
+class Pong(Exception): pass
+class Ping(Exception): pass
+class Close(Exception): pass
+class Closed(Exception): pass
+
+countWebSocketServer = 0
+
+class WebSocketServer(object):
+
+    def __init__(self,soc):
+        assert isinstance(soc,socket.socket),type(soc)
+        self.soc = soc
+        self.fd = soc.fileno()
+        self.verbose = False
+        self.closed  = False
+        self.data = ""
+        self.handshake()
+        global countWebSocketServer
+        countWebSocketServer += 1
+        self.instance = countWebSocketServer
+
+    def __hash__(self):
+        return id(self)
+
+    def fileno(self):
+        return self.fd
+
+    def handshake(self):
+        buff = ""
+        while "\r\n\r\n" not in buff:
+            buff += self.soc.recv(4096)
+        self.lastRecv = time.time()
+        lines = buff.split("\r\n")
+        first = lines.pop(0)
+        self.method,self.loc,self.prot = first.split()
+        self.fields = dict()
+        for line in lines:
+            if line == "": continue
+            k,v = line.split(":",1)
+            k,v = k.strip().lower(),v.strip()
+            self.fields[k] = v
+            if self.verbose:
+                sys.stdout.write("=>%s<=>%s<=" % (k,v))
+                print
+        sig = _sign(self.fields["sec-websocket-key"])
+        out = ""
+        out += "HTTP/1.1 101 Switching Protocols\r\n"
+        out += "Upgrade: websocket\r\n"
+        out += "Connection: Upgrade\r\n"
+        out += "Sec-WebSocket-Accept: %s\r\n" % sig
+        out += "\r\n"
+        self.soc.sendall(out)
+        self.lastSend = time.time()
+        if self.verbose: print("=>%s<=" % out)
+
+    def recvall(self, timeout=None):
+        # returns a list of strings
+        if self.closed: raise Closed()
+        assert self.data == "", self.data
+        out = list()
+        rlist, wlist, xlist = select.select([self.fd],[],[],timeout)
+        if not rlist: # timeout
+            return list()
+        self.data = self.soc.recv(4096)
+        self.lastRecv = time.time()
+        if self.data == "":
+            self.soc.close()
+            self.closed = True
+            return out
+        while self.data != "":
+            try:
+                out.append(self._recv1())
+            except Pong:
+                if self.verbose: print("Pong!")
+            except Close:
+                self.data = ""
+                self.close()
+            except Ping:
+                if self.verbose: print("Ping!")
+        return out
+
+    def close(self):
+        if self.closed: return
+        msg = chr(128 | 8) + chr(0)
+        try:
+            self.soc.sendall(msg)
+        except:
+            pass
+        finally:
+            self.soc.close()
+            self.closed = True
+
+    def ping(self,payload=""):
+        self.send(payload,kind=PING)
+
+    def send(self,payload,kind=TEXT):
+        if self.closed: raise Closed()
+        msg = chr(128 | kind)
+        length = len(payload)
+        if length <= 125:
+            msg += chr(length)
+        else:
+            if length <= 65535:
+                msg += chr(126)
+                msg += struct.pack(">H",length)
+            else:
+                msg += chr(127)
+                msg += struct.pack(">Q",length)
+        msg += str(payload)
+        rlist, wlist, xlist = select.select([],[self.fd],[])
+        self.soc.sendall(msg)
+        self.lastSend = time.time()
+
+    def _recv1(self):
+        assert self.data, "WebSocketServer._recv1: no data?"
+        first = ord(self.data[0])
+        fin = bool(first & 128)
+        opcode = first & 15
+        rsv1 = bool(first & 64)
+        rsv2 = bool(first & 32)
+        rsv3 = bool(first & 16)
+        second = ord(self.data[1])
+        masking = bool(second & 128)
+        len0 = second & 127
+        offset = 2
+        if len0 <= 125:
+            length = len0
+        if len0 == 126:
+            length = struct.unpack("!H",self.data[2:4])[0]
+            offset += 2
+        if len0 == 127:
+            length = struct.unpack("!Q",self.data[2:10])[0]
+            offset += 8
+        if masking:
+            mask = self.data[offset:(offset+4)]
+            offset += 4
+        if self.verbose:
+            print("-------------")
+            print("fin=",fin)
+            print("rsv1=",rsv1)
+            print("rsv2=",rsv2)
+            print("rsv3=",rsv3)
+            print("opcode=",opcode)
+            print("masking=",masking)
+            print("length=",length)
+        while len(self.data) < offset + length:
+            self.data += self.soc.recv(4096)
+            self.lastRecv = time.time()
+            if self.verbose: print("reading more...")
+        payload = self.data[offset:(offset+length)]
+        if len(self.data) == offset + length:
+            if self.verbose: print("perfect length")
+            self.data = ""
+        else:
+            if self.verbose: print("extra stuff")
+            self.data = self.data[(offset+length):len(self.data)]
+        if masking:
+            payload = _unmask(payload,mask)
+        if self.verbose: print("payload=>%s<=" % payload)
+        if opcode == PONG: raise Pong()
+        if opcode == CLOSE: raise Close(payload)
+        if opcode == PING:
+            self.send(payload,PONG)
+            raise Ping()
+        if not fin:
+            if self.data == "":
+                self.data = self.soc.recv(4096)
+                self.lastRecv = time.time()
+            payload += self._recv1()
+        if opcode == BIN:
+            return bytearray(payload)
+        if opcode == TEXT:
+            return payload
+        raise Exception("bad opcode")
