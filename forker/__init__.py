9d760a52884018b46e07a5b54b71311f5147731f
Darin McGill <darin.mcgill@gmail.com>
2016-12-03 14:27:26 -0500

    tweaks

--- forker/__init__.py
+++ forker/__init__.py
@@ -15,38 +15,3 @@ WEB_MAX = 0x1fffffffffffff
 
 __all__ = ["Request", "listen", "WebSocketServer", "TEXT", "BIN", "WEB_MIN", "WEB_MAX"]
 
-
-def main(*args):
-    port = 8080
-    forking = (os.name == 'posix')
-    reporting = False
-    for arg in args:
-        if re.match(r"^\d+$",arg):
-            port = int(arg)
-            continue
-        if arg == "nofork":
-            forking = False
-            continue
-        if arg == "report":
-            reporting = True
-            continue
-        if os.path.exists(arg):
-            os.chdir(arg)
-            continue
-    for sock, addr in listen(port, forking):
-        request = Request(sock=sock, remote_ip=addr[0])
-        if reporting:
-            out = b"HTTP/1.0 200 OK\r\n"
-            out += b"Content-type: text/plain\r\n\r\n"
-            out += bytes(request)
-            sock.sendall(out)
-            print(request)
-        else:
-            sock.sendall(request.serve())
-        sock.close()
-        if forking:
-            sys.exit(0)
-
-
-if __name__ == "__main__":
-    main(*sys.argv[1:])
---
+++ forker/__main__.py
@@ -0,0 +1,41 @@
+from .Request import Request
+from .listen import listen
+import sys
+import os
+import re
+
+
+def main(*args):
+    port = 8080
+    forking = (os.name == 'posix')
+    reporting = False
+    for arg in args:
+        if re.match(r"^\d+$", arg):
+            port = int(arg)
+            continue
+        if arg == "nofork":
+            forking = False
+            continue
+        if arg == "report":
+            reporting = True
+            continue
+        if os.path.exists(arg):
+            os.chdir(arg)
+            continue
+    for sock, addr in listen(port, forking):
+        request = Request(sock=sock, remote_ip=addr[0])
+        if reporting:
+            out = b"HTTP/1.0 200 OK\r\n"
+            out += b"Content-type: text/plain\r\n\r\n"
+            out += bytes(request)
+            sock.sendall(out)
+            print(request)
+        else:
+            sock.sendall(request.serve())
+        sock.close()
+        if forking:
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
